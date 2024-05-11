31ff1603c2dd6d9b18422dace5243b37eff3bd83
robiulsagor <robiul100.me@gmail.com>
2024-05-11 09:50:05 +0600

    bookings page done

---
+++ app/bookings/page.js
@@ -0,0 +1,28 @@
+import PastBookings from "@/components/profile/PastBookings";
+import ProfileInfo from "@/components/profile/ProfileInfo";
+import UpcommingBookings from "@/components/profile/UpcommingBookings";
+
+export default function BookingsPage() {
+  return (
+    <>
+      <div>
+        <section className="mt-[100px]">
+          <div className="container">
+            <ProfileInfo />
+          </div>
+        </section>
+        <section>
+          <div className="container">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+              {/* Past Bookings */}
+
+              <PastBookings />
+
+              <UpcommingBookings />
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
