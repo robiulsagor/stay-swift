5f6c2c2fb37fc1e21a1c90d3c0da2b7c91d912c4
robiulsagor <robiul100.me@gmail.com>
2024-05-12 08:11:35 +0600

    added dynamic profile info

--- components/profile/ProfileInfo.jsx
+++ components/profile/ProfileInfo.jsx
@@ -1,14 +1,30 @@
-export default function ProfileInfo() {
+import { auth } from "@/auth";
+import Image from "next/image";
+
+export default async function ProfileInfo() {
+  const session = await auth();
   return (
     <div className="flex flex-col items-center py-8 text-center">
       {/* profile image */}
       <div className="relative max-h-[180px] max-w-[180px] rounded-full lg:mb-8 h-[100px] w-[100px] bg-orange-600 grid place-items-center text-4xl text-white">
-        S
+        {session?.user?.image ? (
+          <Image
+            src={session?.user?.image}
+            width={100}
+            height={100}
+            className="rounded-full"
+            alt="user img"
+          />
+        ) : (
+          session?.user?.name.charAt(0)
+        )}
       </div>
       {/* name , email */}
       <div>
-        <h3 className="text-2xl font-semibold lg:text-[28px]">Sumit Saha</h3>
-        <p className="leading-[231%] lg:text-lg">sumitsaha@gmail.com</p>
+        <h3 className="text-2xl font-semibold lg:text-[28px]">
+          {session?.user?.name}{" "}
+        </h3>
+        <p className="leading-[231%] lg:text-lg">{session?.user?.email}</p>
       </div>
       <div className="w-3/4 border-b border-[#a4a4a4] py-6 lg:py-4" />
     </div>
